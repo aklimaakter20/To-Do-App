7ee090bd48019a3d83d77cc90349df7cf1b1e0cd
aklimaakter20 <aklimaakter20@gmail.com>
2025-03-04 16:17:30 +0600

    localstorage data save and show work completed

--- script.js
+++ script.js
@@ -14,14 +14,25 @@ const inputBox = document.getElementById("input-box")
         li.appendChild(span)
     }
     inputBox.value = ""
-     
+    saveData();
    }
    listContainer.addEventListener("click", function(e){
     if(e.target.tagName === "LI"){
         e.target.classList.toggle("checked");
+        saveData();
     }else if( e.target.tagName === "SPAN") {
         e.target.parentElement.remove();
+        saveData();
         }
     }, false);
-    
-   
+
+    // data save of  localStorage
+   function saveData(){
+    localStorage.setItem("data",listContainer.innerHTML);
+   }
+//    data show of localStorage
+   function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+
+   }
+   showTask()
